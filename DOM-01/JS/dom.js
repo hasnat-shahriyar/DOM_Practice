cf58f580f7e3de5b26043bb62d877dbe852fe1bb
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-09-30 21:07:09 +0600

    dom practice

---
+++ DOM-01/JS/dom.js
@@ -0,0 +1,9 @@
+const sections = document.querySelectorAll ("section");
+
+for (const section of sections){
+    section.style.color = "tomato";
+    section.style.border = "1px solid tomato";
+    section.style.width = "500px";
+    section.style.textAlign = "center";
+    section.style.backgroundColor = "#030345";
+}
