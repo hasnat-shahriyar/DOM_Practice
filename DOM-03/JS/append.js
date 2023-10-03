e3be00a684279f8607b3d35bbc50c07382f58bc7
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-03 13:16:42 +0600

    inserting new section and list items

---
+++ DOM-03/JS/append.js
@@ -0,0 +1,49 @@
+// Step 1: Where to add
+const sectionOne = document.querySelector(".section_One"); // Use querySelector to select the class
+
+const divisionList = document.getElementById("division");
+
+// Step 2: What to be added
+const h1 = document.createElement("h1");
+h1.innerText = "Northern Bangladesh";
+
+// Insert "Northern Bangladesh" at the top of the section
+sectionOne.insertBefore(h1, divisionList);
+
+const li = document.createElement("li");
+li.innerText = "Sylhet";
+
+// Step 3: Add the Child
+divisionList.appendChild(li);
+
+
+/*----------------------------*/
+// Step 1: Where to add
+const parentContainer = document.getElementById("parent");
+
+// Step 2: What to be added
+const section = document.createElement("section");
+const h1Southern = document.createElement("h1"); // Rename h1 to h1Southern
+h1Southern.innerText = "Southern Bangladesh";
+section.appendChild(h1Southern); // Use h1Southern instead of h1
+
+const ul = document.createElement("ul");
+
+const liOne = document.createElement("li");
+liOne.innerText = "Khulna";
+ul.appendChild(liOne);
+
+const liTwo = document.createElement("li");
+liTwo.innerText = "Barisal";
+ul.appendChild(liTwo);
+
+const liThree = document.createElement("li");
+liThree.innerText = "Dhaka";
+ul.appendChild(liThree);
+
+const liFour = document.createElement("li");
+liFour.innerText = "Chittagong";
+ul.appendChild(liFour);
+
+section.appendChild(ul);
+parentContainer.appendChild(section);
