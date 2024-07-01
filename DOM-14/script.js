093ea0594a5e5ebb33de7b34e75bee57886d0aaa
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-07-01 18:11:13 +0600

    localStorage

--- DOM-14/script.js
+++ DOM-14/script.js
@@ -1,23 +1,26 @@
 // Function to add item to local storage
 const addToLocalStorage = () => {
-    let InputNumberItem = document.getElementById("key_number");
-    let InputNumber = InputNumberItem.value;
+  let InputNumberItem = document.getElementById("key_number");
+  let InputNumber = InputNumberItem.value;
 
-    let inputItem = document.getElementById("input_area");
-    let inputText = inputItem.value;
+  let inputItem = document.getElementById("input_area");
+  let inputText = inputItem.value;
 
-    localStorage.setItem(InputNumber, inputText);
-}
+  localStorage.setItem(InputNumber, inputText);
+};
 
 // Function to remove item from local storage
 const removeFromLocalStorage = () => {
-    let InputNumberItem = document.getElementById("key_number");
-    let InputNumber = InputNumberItem.value;
+  let InputNumberItem = document.getElementById("key_number");
+  let InputNumber = InputNumberItem.value;
 
-    localStorage.removeItem(InputNumber);
-}
-
+  localStorage.removeItem(InputNumber);
+};
 
 // Add event listeners after defining functions
-document.getElementById("add_button").addEventListener("click", addToLocalStorage);
-document.getElementById("remove_button").addEventListener("click", removeFromLocalStorage);
+document
+  .getElementById("add_button")
+  .addEventListener("click", addToLocalStorage);
+document
+  .getElementById("remove_button")
+  .addEventListener("click", removeFromLocalStorage);
